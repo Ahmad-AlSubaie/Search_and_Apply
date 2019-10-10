f44913f79b857ab409803b90b4c2f3b5ae9d08a6
TheAlienBee <ahmadalsubaie51@gmail.com>
2019-10-09 20:40:40 -0400

    scrapping finaly started, this just gets the job titles
    Signed-off-by: Ahmad Alsubaie <ahmadalsubaie51@gmail.com>

--- Search_and_Apply/Search_and_Apply/spiders/IndeedSpider.py
+++ Search_and_Apply/Search_and_Apply/spiders/IndeedSpider.py
@@ -1,24 +1,20 @@
 import scrapy
 
-start_urls = ['https://www.indeed.com/']
-
 
 class IndeedSpider(scrapy.Spider):
-	name = 'Indeed'
-	
-	def parse(self, response):
+    name = "IndeedSpider"
 
-		return scrapy.FormRequest.from_response(
-			response,
-			formid='text-input-what',
-			formname = 'q',
-			formdata={'what': 'CS'}
-		)
 
-		
-def main():
-	indeedSpider = IndeedSpider()
-	response = scrapy.Request(url = start_urls[0])
-	indeedSpider.parse(response)
-	
-main()
+    def start_requests(self):
+        start_urls = ["https://www.indeed.com/"]
+        yield scrapy.Request(url=start_urls[0], callback=self.parse)
+
+    def parse(self, response):
+      return scrapy.FormRequest.from_response(
+            response,
+            clickdata=None,
+            formdata={'q': 'CS'},
+            callback=self.getTitle
+        )
+    def getTitle(self, response):
+      print(response.css('div.title').xpath("./a/@title").getall())
