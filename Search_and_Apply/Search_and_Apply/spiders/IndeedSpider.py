beb64b40d2ad5014541e571e22c7388c4b63c02b
TheAlienBee <ahmadalsubaie51@gmail.com>
2019-10-21 11:51:52 -0400

    Semi working scrapy parser, can create links to Jobs run with CMD scrapy runspider IndeedSpider.py -o test.json
    Signed-off-by: Ahmad Alsubaie <ahmadalsubaie51@gmail.com>

--- Search_and_Apply/Search_and_Apply/spiders/IndeedSpider.py
+++ Search_and_Apply/Search_and_Apply/spiders/IndeedSpider.py
@@ -1,9 +1,8 @@
 import scrapy
-
+from scrapy.crawler import CrawlerProcess
 
 class IndeedSpider(scrapy.Spider):
     name = "IndeedSpider"
-
 
     def start_requests(self):
         start_urls = ["https://www.indeed.com/"]
@@ -13,8 +12,75 @@ class IndeedSpider(scrapy.Spider):
       return scrapy.FormRequest.from_response(
             response,
             clickdata=None,
-            formdata={'q': 'CS'},
-            callback=self.getTitle
+            formdata={'q': 'software engineer'},
+            callback=self.getJob
         )
-    def getTitle(self, response):
-      print(response.css('div.title').xpath("./a/@title").getall())
+    def getJob(self, response):
+      Titles = response.css('div.title').xpath("./a/@title").getall()
+      Links = response.css('div.title').xpath("./a/@href").getall()
+      data = []
+
+      for i in range(len(Links)):
+        data.append(Job(
+            Title = Titles[i],
+            Link = Links[i]))
+      return data
+
+class Job(scrapy.Item):
+   Title = scrapy.Field()
+   Link = scrapy.Field()
+
+
+process = CrawlerProcess(settings={
+    'FEED_FORMAT': 'json',
+    'FEED_URI': 'items.json'
+})
+
+process.crawl(IndeedSpider)
+process.start()
+
+
+
+
+
+S = IndeedSpider()
+print(IndeedSpider.start_requests())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
