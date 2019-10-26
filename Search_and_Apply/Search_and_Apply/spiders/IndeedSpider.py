96f38679f76bf83cda679a98a77bd3ccd0acb54f
TheAlienBee <ahmadalsubaie51@gmail.com>
2019-10-26 19:17:00 -0400

    update
    Signed-off-by: Ahmad Alsuabie <ahmadalsubaie51@gmail.com>

--- Search_and_Apply/Search_and_Apply/spiders/IndeedSpider.py
+++ Search_and_Apply/Search_and_Apply/spiders/IndeedSpider.py
@@ -1,6 +1,15 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support import ui
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.support import expected_conditions as EC
+
+
 class IndeedSpider(scrapy.Spider):
     name = "IndeedSpider"
 
@@ -23,7 +32,8 @@ class IndeedSpider(scrapy.Spider):
       for i in range(len(Links)):
         data.append(Job(
             Title = Titles[i],
-            Link = Links[i]))
+            Link = "https://www.indeed.com" +  Links[i]))
+
       return data
 
 class Job(scrapy.Item):
@@ -31,56 +41,31 @@ class Job(scrapy.Item):
    Link = scrapy.Field()
 
 
+class ApplySpider(scrapy.Spider):
+
+    name = "ApplySpider"
+
+    #def __init__(self):
+        #self.driver = webdriver.Chrome()
+
+    def start_requests(self):
+      start_urls = ["https://www.indeed.com/rc/clk?jk=4f732a1edb258a0b&fccid=bdbc38ad07a162e4&vjs=3"]
+      yield scrapy.Request(url=start_urls[0], callback=self.parse)
+
+    def parse(self, response):
+      return scrapy.FormRequest.from_response(
+            response,
+            clickdata=None,
+            formdata={'applicant.name': 'John Smith','applicant.email': 'applysmith2345@gmail.com' },
+            callback = self.open
+        )
+    def open(self, response):
+      response.open_in_browser(response)
+
+
 process = CrawlerProcess(settings={
     'FEED_FORMAT': 'json',
-    'FEED_URI': 'items.json'
+    'FEED_URI': 'jobs.json'
 })
 
-process.crawl(IndeedSpider)
-process.start()
-
-
-
-
-
-S = IndeedSpider()
-print(IndeedSpider.start_requests())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+process.crawl(ApplySpider)
