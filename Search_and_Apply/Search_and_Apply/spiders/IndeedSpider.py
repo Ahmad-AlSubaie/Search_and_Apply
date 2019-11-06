dbca71c43900c57e032598db2240899e2b5886fb
TheAlienBee <ahmadalsubaie51@gmail.com>
2019-11-06 18:16:07 -0500

    Signed-off-by: TheAlienBee <ahmadalsubaie51@gmail.com>
    big update

--- Search_and_Apply/Search_and_Apply/spiders/IndeedSpider.py
+++ Search_and_Apply/Search_and_Apply/spiders/IndeedSpider.py
@@ -1,29 +1,45 @@
 import scrapy
+from scrapy.utils.response import open_in_browser
 from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
 
-#from selenium import webdriver
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.support import ui
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.common.keys import Keys
-#from selenium.webdriver.common.action_chains import ActionChains
-#from selenium.webdriver.support import expected_conditions as EC
+from scrapy.http import HtmlResponse
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import ui
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class IndeedSpider(scrapy.Spider):
     name = "IndeedSpider"
 
+    custom_settings ={
+        'FEED_FORMAT': 'json',
+        'FEED_URI': 'URLs_from_IndeedSpider.json',
+        "TELNETCONSOLE_PORT" : None
+        }
+
+    start_urls = ["https://www.indeed.com/"]
+
+    def __init__(self, searchVar):
+      self.search=searchVar
+
+
     def start_requests(self):
-        start_urls = ["https://www.indeed.com/"]
-        yield scrapy.Request(url=start_urls[0], callback=self.parse)
+      yield scrapy.Request(url=self.start_urls[0], callback=self.parse)
 
     def parse(self, response):
-      return scrapy.FormRequest.from_response(
+        yield scrapy.FormRequest.from_response(
             response,
             clickdata=None,
-            formdata={'q': 'software engineer'},
+            formdata={'q': self.search},
             callback=self.getJob
         )
+
     def getJob(self, response):
       Titles = response.css('div.title').xpath("./a/@title").getall()
       Links = response.css('div.title').xpath("./a/@href").getall()
@@ -36,6 +52,7 @@ class IndeedSpider(scrapy.Spider):
 
       return data
 
+
 class Job(scrapy.Item):
    Title = scrapy.Field()
    Link = scrapy.Field()
@@ -45,27 +62,50 @@ class ApplySpider(scrapy.Spider):
 
     name = "ApplySpider"
 
-    #def __init__(self):
-        #self.driver = webdriver.Chrome()
+    def __init__(self, linkVar, name, email):
+      self.start_urls = linkVar
+      self.username = name
+      self.email = email
+      self.options = webdriver.ChromeOptions()
+      self.options.add_argument('headless')
+      self.options.add_argument('window-size=1200x600')
+      self.driver = webdriver.Chrome(chrome_options=self.options)
+
 
     def start_requests(self):
-      start_urls = ["https://www.indeed.com/rc/clk?jk=4f732a1edb258a0b&fccid=bdbc38ad07a162e4&vjs=3"]
-      yield scrapy.Request(url=start_urls[0], callback=self.parse)
+      yield scrapy.Request(url=self.start_urls[0], callback=self.parse)
+
 
     def parse(self, response):
+      open_in_browser(response)
+      print("\n"+ response.url+ "\n")
+
+
+      self.driver.get(response.url)
+
+      apply_button = self.driver.find_element_by_xpath('//*[@id="indeedApplyButtonContainer"]/span/div[2]/button/div')
+      apply_button.click()
+      WebDriverWait(self.driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.NAME, "Apply Now")))
+      self.driver.get_screenshot_as_file('debug.png')
+
+
       return scrapy.FormRequest.from_response(
             response,
             clickdata=None,
-            formdata={'applicant.name': 'John Smith','applicant.email': 'applysmith2345@gmail.com' },
-            callback = self.open
+            formdata={'applicant.name': self.name,'applicant.email': self.email }
         )
-    def open(self, response):
-      response.open_in_browser(response)
 
+def searchFor(searchVar):
 
-process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'json',
-    'FEED_URI': 'jobs.json'
-})
+    settings = Settings()
+    process = CrawlerProcess(settings)
+    settings.set('DOWNLOADER_MIDDLEWARES', None)
+    for item in searchVar:
+      process.crawl(IndeedSpider, item)
+    process.start()
 
-process.crawl(ApplySpider)
+def applyTo(applyVar):
+    settings = Settings()
+    process = CrawlerProcess(settings)
+    process.crawl(ApplySpider, applyVar[0], applyVar[1], applyVar[2])
+    process.start()
---
+++ driver.py
@@ -0,0 +1,20 @@
+from Search_and_Apply.Search_and_Apply.spiders.IndeedSpider import searchFor, applyTo
+
+
+def main():
+
+  #get user input for search terms
+  #save search tems to list_to_search
+
+  list_to_search = ['software engineer', 'art']
+  #searchFor(list_to_search)
+
+  #open file URLs_from_IndeedSpider
+  #save relavent links to list_of_links
+
+  list_of_links = ['https://www.indeed.com/viewjob?cmp=Catalyte&t=Become+Software+Developer+No+Experience&jk=1bcef3d9fc1b0b32&sjdu=QwrRXKrqZ3CNX5W-O9jEvW16Yhk4ozNCdaWKrMunypsF4bpKjscnMnzPFjGwUT8pmTq0NikKnfo91oL8LEaz9PmFE3FVkgBqhjy5aD2O7I6Oveef6XgOIAsWlCpqPX_L&adid=321198033&pub=4a1b367933fd867b19b072952f68dceb&vjs=3']
+
+  applyTo([list_of_links , 'John Smith', 'applysmith2345@gmail.com'])
+
+
+main()
