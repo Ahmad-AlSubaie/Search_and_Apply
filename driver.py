ed89d5d9ad04e0bdf0c2e025f64d5cdcfd1ffb66
TheAlienBee <32343365+TheAlienBee@users.noreply.github.com>
2019-11-18 17:52:49 -0500

    Added application filling code to core, cleaned up internal data scraping format, added fetch for company names

--- driver.py
+++ driver.py
@@ -1,6 +1,6 @@
 from Search_and_Apply.Search_and_Apply.spiders.IndeedSpider import searchFor
 from Search_and_Apply.Search_and_Apply.IndeedExpressApply import ExpressApply
-import json
+
 
 
 def main():
@@ -11,28 +11,17 @@ def main():
   list_of_links = ['https://www.indeed.com/viewjob?cmp=Catalyte&t=Become+Software+Developer+No+Experience&jk=1bcef3d9fc1b0b32&sjdu=QwrRXKrqZ3CNX5W-O9jEvW16Yhk4ozNCdaWKrMunypsF4bpKjscnMnzPFjGwUT8pmTq0NikKnfo91oL8LEaz9PmFE3FVkgBqhjy5aD2O7I6Oveef6XgOIAsWlCpqPX_L&adid=321198033&pub=4a1b367933fd867b19b072952f68dceb&vjs=3']
 
 
-<<<<<<< Updated upstream
-  list_to_search = ['software engineer', 'art']
-  searchFor(list_to_search, list_of_links) #and writes the resultes to URLs_from_IndeedSpider
-=======
   applyBot = ExpressApply('John Smith', 'applysmith2345@gmail.com')
->>>>>>> Stashed changes
 
   list_to_search = ['art']
 
   searchFor(list_to_search)
 
-<<<<<<< Updated upstream
-  applyTo([list_of_links , 'John Smith', 'applysmith2345@gmail.com'])
-
-
-
-=======
   list_of_links = ['https://www.indeed.com/viewjob?cmp=Catalyte&t=Become+Software+Developer+No+Experience&jk=1bcef3d9fc1b0b32&sjdu=QwrRXKrqZ3CNX5W-O9jEvW16Yhk4ozNCdaWKrMunypsF4bpKjscnMnzPFjGwUT8pmTq0NikKnfo91oL8LEaz9PmFE3FVkgBqhjy5aD2O7I6Oveef6XgOIAsWlCpqPX_L&adid=321198033&pub=4a1b367933fd867b19b072952f68dceb&vjs=3']
 
   applyBot.applyTo([list_of_links ])
 
 
   input("press any button to close...\n")
->>>>>>> Stashed changes
+
 main()
