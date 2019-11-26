019b768be1d442db85d9496b0e97b9d30bd08c00
TheAlienBee <32343365+TheAlienBee@users.noreply.github.com>
2019-11-26 18:22:59 -0500

    Added selenium tests

--- testSuit.py
+++ testSuit.py
@@ -28,7 +28,7 @@ class TestFileContent(unittest.TestCase):
 
 
 
-class SpiderFileTester  (unittest.TestCase):
+class SpiderFileTester(unittest.TestCase):
 
     def SpiderHasLinks(self):
         with jsonlines.open("URLs_from_IndeedSpider.json", mode ='r') as reader:
@@ -44,6 +44,45 @@ class SpiderFileTester  (unittest.TestCase):
         except Exception as e2:
             self.fail()
 
+
+
+
+
+class IndeedForumFillTester(unittest.TestCase):
+
+    applyBot = None
+
+    def setUp(self):
+         self.applyBot = ExpressApply()
+
+    def applyWithNoLink(self):
+        self.applyBot.addEmail('testEmail')
+        self.applyBot.addName('jhon Smith')
+        try:
+            self.applyBot.applyTo()
+        except Exception as e:
+            self.fail()
+
+    def addEmail(self):
+        self.applyBot.addEmail('testEmail')
+
+    def addName(self):
+        self.applyBot.addName('testEmail')
+
+    def applyCorrectly(self):
+        self.applyBot.addEmail('testEmail')
+        self.applyBot.addName('jhon Smith')
+        L = 'https://www.indeed.com/viewjob?cmp=Havertys-Furniture-Companies&t=Retail+Sales+Consultant&jk=e5d59f3d51f031db&sjdu=QwrRXKrqZ3CNX5W-O9jEvZ793ZkrMSjbJmKrDbvG6ZioyIIOjzV35vrUfAIrEsV0Zu8NScqbgMmjtEe_wKP8jCzsP4fIgOrPqgPR449NdBo&adid=322682335&pub=4a1b367933fd867b19b072952f68dceb&vjs=3'
+        try:
+            self.applyBot.applyTo(L)
+        except Exception as e:
+            self.fail()
+    def applyWithNoProfileTest(self):
+        L = 'https://www.indeed.com/viewjob?cmp=Havertys-Furniture-Companies&t=Retail+Sales+Consultant&jk=e5d59f3d51f031db&sjdu=QwrRXKrqZ3CNX5W-O9jEvZ793ZkrMSjbJmKrDbvG6ZioyIIOjzV35vrUfAIrEsV0Zu8NScqbgMmjtEe_wKP8jCzsP4fIgOrPqgPR449NdBo&adid=322682335&pub=4a1b367933fd867b19b072952f68dceb&vjs=3'
+        try:
+            self.applyBot.applyTo(L)
+        except Exception as e:
+            self.fail()
 
 
 
